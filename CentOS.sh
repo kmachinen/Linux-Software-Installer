27d36a72a3e7872e451325e58a20b54525a72542
Kiran Kumar Machineni <26577324+kmachinen@users.noreply.github.com>
2025-04-03 20:06:39 -0400

    Update CentOS.sh

--- CentOS.sh
+++ CentOS.sh
@@ -72,3 +72,9 @@ go version
 
 # install kind(k8s on docker)
 go install sigs.k8s.io/kind@v0.27.0 && kind create cluster
+
+#install kubectl cli
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
